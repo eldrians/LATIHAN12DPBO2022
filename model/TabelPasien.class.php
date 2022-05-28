b0140691c506b14083abec3b5b26ec157cbcf04f
Axel Eldrian <91566708+eldrians@users.noreply.github.com>
2022-05-28 07:20:19 +0700

    Update TabelPasien.class.php

--- model/TabelPasien.class.php
+++ model/TabelPasien.class.php
@@ -13,4 +13,24 @@ class TabelPasien extends DB
 		// Mengeksekusi query
 		return $this->execute($query);
 	}
+	
+	function getDetailPasien($id) {
+		$query = "SELECT * FROM pasien WHERE id='$id'";
+		return $this->execute($query);
+	}
+
+	function createPasien($data) {
+		$query = "INSERT INTO pasien VALUES (null, '" . $data->getNik() . "', '" . $data->getNama() . "', '" . $data->getTempat() . "', '" . $data->getTl() . "', '" . $data->getGender() . "', '" . $data->getEmail() . "', '" . $data->getTelp() . "')";
+		return $this->execute($query);
+	}
+
+	function updatePasien($data) {
+		$query = "UPDATE pasien SET nik='" . $data->getNik() . "', nama='" . $data->getNama() . "', tempat='" . $data->getTempat() . "', tl='" . $data->getTl() . "', gender='" . $data->getGender() . "', email='" . $data->getEmail() . "', telp='" . $data->getTelp() . "' WHERE id='" . $data->getId() . "'";
+		return $this->execute($query);
+	}
+	
+	function deletePasien($id) {
+		$query = "DELETE FROM pasien WHERE id='$id'";
+		return $this->execute($query);
+	}
 }
